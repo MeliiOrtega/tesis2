0baed5697cc430bd483369a572f3b97c322f828f
Genesis Ortega <genesisortega60@gmail.com>
2021-09-10 03:12:25 -0500

    appe

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL'),
+    /* 'url' => env('APP_URL'), */
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => url('/'),
 
     /*
     |--------------------------------------------------------------------------
